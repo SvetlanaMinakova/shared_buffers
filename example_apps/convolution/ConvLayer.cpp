eeb12b71f5526ffa0d1f9a44484690ea3cbf366d
Svetlana <svetlanaminakova42@gmail.com>
2022-04-07 20:21:01 +0200

    convolutional example deleted due to its complexity that has nothing to do with mutexes

--- example_apps/convolution/ConvLayer.cpp
+++
@@ -1,9 +0,0 @@
-//
-// Created by svetlana on 06/04/2022.
-//
-
-#include "ConvLayer.h"
-
-void ConvLayer::setKernel(float *kernel) {
-    this->kernel = kernel;
-}
--- example_apps/convolution/detectEdges/single_buf/DetectEdgesSB.cpp
+++
@@ -1,9 +0,0 @@
-//
-// Created by svetlana on 07/04/2022.
-//
-
-#include "DetectEdgesSB.h"
-
-int DetectEdgesSB::run() {
-    return ConvExample::run();
-}
